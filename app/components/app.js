b3c372cdb0f1a0042a4554c27c711e67ba5ab9cc
jzhang729 <jordanzhang@gmail.com>
2015-08-11 20:12:49 -0700

    Added Bootstrap and Navbar

--- app/components/app.js
+++ app/components/app.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Timelapse from './timelapse'
 import Snapshot from './snapshot'
+import Navbar from './navbar'
 
 export default React.createClass({
   render: function() {
     return (
-      <div>
-        <h1>Hello world</h1>
+      <div className="">
+        <Navbar />
         <Timelapse /><br />
         <Snapshot />
       </div>
--- app/main.js
+++ app/main.js
@@ -2,10 +2,11 @@ var css = require('!css!sass!./main.scss');
 // => returns compiled css code from file.scss, resolves imports and url(...)s
 require('!style!css!sass!./main.scss');
 
-
 import React from 'react'
 import App from './components/app.js'
 
+
+// React.render(<Navbar />, document.getElementById('navbar'))
 React.render(<App />, document.getElementById('content'))
 
 console.log('Application is loaded!');
