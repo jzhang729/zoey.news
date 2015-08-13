a373070a84eb9891e22be99f2f58a63944fd2598
jzhang729 <jordanzhang@gmail.com>
2015-08-12 23:26:45 -0700

    Updated sidebar style

--- app/components/leftbar.js
+++ app/components/leftbar.js
@@ -4,9 +4,20 @@ export default React.createClass({
   render: function() {
     return (
       <div className="left-side">
-        <p>
-          To go on account rope's end schooner warp lee list tack cable haul wind reef sails lugsail spanker scurvy bilge rat Pirate Round chase guns brigantine square-rigged fire in the hole Sail ho execution dock spyglass dance the hempen jig red ensign bounty yardarm splice the main brace snow topgallant Chain Shot measured fer yer chains squiffy knave tackle Arr spirits handsomely Jack Tar heave down league jack scourge of the seven seas driver pirate Blimey scuttle case shot quarterdeck weigh anchor barkadeer rum dead men tell no tales cutlass line grog Gold Road bilge water belay boatswain Corsair coxswain piracy mutiny man-of-war transom holystone me rutters mizzenmast crow's nest walk the plank scallywag jolly boat six pounders landlubber or just lubber swing the lead sutler cog keel careen killick yard bilged on her anchor capstan port chantey heave to Privateer starboard avast shrouds Yellow Jack parley provost yawl clap of thunder hulk Shiver me timbers sloop pink. Pressgang hornswaggle Blimey tackle lugger boom bilge water smartly mutiny fluke hulk topmast Letter of Marque handsomely bounty Privateer crow's nest coxswain splice the main brace league chase chase guns take a caulk lass interloper hogshead Sink me doubloon jolly boat reef rope's end fathom line Jack Ketch parley long boat crimp topsail broadside schooner swing the lead galleon squiffy log booty swab Arr walk the plank clap of thunder spanker reef sails keel American Main jury mast scurvy man-of-war yardarm cable gunwalls long clothes lateen sail bilged on her anchor cog crack Jennys tea cup loaded to the gunwalls Barbary Coast draught ho scuppers knave tack topgallant lad to go on account mizzen Jolly Roger careen provost scuttle yo-ho-ho pinnace brigantine Gold Road carouser heave down Chain Shot overhaul black jack aft hearties barque aye Jack Tar fire in the hole mizzenmast square-rigged Yellow Jack scallywag avast lanyard. Barque stern mizzenmast transom bucko hang the jib chandler Blimey topmast ahoy lad gaff list keel dead men tell no tales lookout chantey nipper parley maroon cackle fruit carouser
-        </p>
+
+        <div>
+          Blue
+        </div>
+        <div>
+          Red
+        </div>
+        <div>
+          Yellow
+        </div>
+        <div>
+          Purple
+        </div>
+        
       </div>
     )
   }
