8401ca5297cd24444abdf9bf2275534c503a5238
mattfoulger <mfoulger@gmail.com>
2015-08-15 19:51:18 -0700

    can control state of slider component by dragging handles

--- app/components/slider.js
+++ app/components/slider.js
@@ -3,14 +3,21 @@ import ReactSlider from 'react-slider'
 
 export default React.createClass({
   
+  getInitialState: function() {
+    return {
+      value: [10,80]
+    }
+  },
+
   handleDateChange: function() {
-    console.log("yo")
+    console.log("hey")
+    console.log(this.state.value)
   },
 
   render: function() {
     var slider 
     return (
-      <ReactSlider className='horizontal-slider' defaultValue={[0, 100]} withBars onChange={this.handleDateChange} />
+      <ReactSlider className='horizontal-slider' value={this.state.value} withBars onChange={this.handleDateChange} />
     )
   }
 
