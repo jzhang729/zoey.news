5e0a6e92653d1179267ba04d7d155384ce3af076
jzhang729 <jordanzhang@gmail.com>
2015-08-17 09:33:49 -0700

    Updated navbar for mobile responsiveness and name

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -7,23 +7,25 @@ export default React.createClass({
   render: function() {
     return (
       <div className="header">
-        <Navbar brand={<a href="#">React-Bootstrap</a>}>
-          <Nav>
-            <NavItem eventKey={1} href='#'>Link</NavItem>
-            <NavItem eventKey={2} href='#'>Link</NavItem>
-            <DropdownButton eventKey={3} title='Dropdown'>
-              <MenuItem eventKey='1'>Action</MenuItem>
-              <MenuItem eventKey='2'>Another action</MenuItem>
-              <MenuItem eventKey='3'>Something else here</MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey='4'>Separated link</MenuItem>
-            </DropdownButton>
-          </Nav>
-        </Navbar>
+        <Navbar brand='Zoey' toggleNavKey={0}>
+            <Nav right eventKey={0}> {/* This is the eventKey referenced */}
+              <NavItem eventKey={1} href='#'>Sign Up</NavItem>
+              <NavItem eventKey={2} href='#'>Log In</NavItem>
+              
+            </Nav>
+          </Navbar>
       </div>
     )
   }
 })
+
+// <DropdownButton eventKey={3} title='Dropdown'>
+//                 <MenuItem eventKey='1'>Action</MenuItem>
+//                 <MenuItem eventKey='2'>Another action</MenuItem>
+//                 <MenuItem eventKey='3'>Something else here</MenuItem>
+//                 <MenuItem divider />
+//                 <MenuItem eventKey='4'>Separated link</MenuItem>
+//               </DropdownButton>
 
 
 // <TabbedArea defaultActiveKey={1} animation={false}>
