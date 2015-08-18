9d2f7941b4260585b81a6f4b50a27cd676eb61c0
Jon Major <MajorJ@users.noreply.github.com>
2015-08-18 12:27:54 -0700

    Update addkeyword.js

--- app/components/addkeyword.js
+++ app/components/addkeyword.js
@@ -42,41 +42,8 @@ export default React.createClass({
     this.setState({value: event.target.value})
   },
 
-  // handleRemoveKeyword: function(keywordIndex) {
-  //   this.getFlux().actions.removeKeyword(this.props.chartID, keywordIndex);
-  // },
-
   render: function() {
-    // var divStyle = [{
-    //   borderBottomColor: Color.Fill[0],
-    //   borderBottomWidth: '8px',
-    //   borderBottomStyle: 'solid'
-    // },{
-    //   borderBottomColor: Color.Fill[1],
-    //   borderBottomWidth: '8px',
-    //   borderBottomStyle: 'solid'
-    // },{
-    //   borderBottomColor: Color.Fill[2],
-    //   borderBottomWidth: '8px',
-    //   borderBottomStyle: 'solid'
-    // }];
-    // var keywordList = this.props.list.map(function(keyword, keywordIndex){
-    //   if (this.props.legend == true) {
-    //     return (
-    //       <li className="keyword-list-item" style={divStyle[keywordIndex]}>
-    //         {keyword} &nbsp;
-    //         <i className="fa fa-times-circle-o" chartID={this.props.chartID} onClick={this.handleRemoveKeyword.bind(this, keywordIndex)}></i>
-    //       </li>
-    //     )
-    //   } else {
-    //     return (
-    //       <li className="keyword-list-item">
-    //         {keyword} &nbsp;
-    //         <i className="fa fa-times-circle-o" chartID={this.props.chartID} onClick={this.handleRemoveKeyword.bind(this, keywordIndex)}></i>
-    //       </li>
-    //     )
-    //   }
-    // }.bind(this))
+    
     return (
         <div>
           <div className="keyword-add">
